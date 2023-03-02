74ca9bf3cc084298dff99f3f696b511b044ab449
Caspar Brown <casparmoab@gmail.com>
2023-03-02 12:10:51 -0700

    Added datasets and test examples. Updated Scripts

---
+++ face_recognition_videos.py
@@ -0,0 +1,9 @@
+import pickle
+import cv2
+
+
+from utils import face_rects
+from utils import face_encodings
+from utils import nb_of_matches
+
+
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import dlib
-from glob import glob
+import pickle
 import cv2
-import numpy as np
 import os
+
+import face_encoding
+import face_recognition_images
+import face_recognition_videos
+import utils
+
+# load the encodings + names dictionary
+with open("encodings.pickle", "rb") as f:
+    name_encodings_dict = pickle.load(f)
+
+
+# load the input image
+image = cv2.imread("examples/ts1.jpeg")
+# get the 128-d face embeddings for each face in the input image
+encodings = utils.face_encodings(image)
