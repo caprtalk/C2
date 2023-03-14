65f77d0969d5c543f7b5816821271ac1120f43c5
Caspar Brown <casparmoab@gmail.com>
2023-03-14 12:19:15 -0600

    tests/edits

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ import face_recognition_images
 import face_recognition_active
 import utils
 
-# load the encodings + names dictionary
-with open("encodings.pickle", "rb") as f:
-    name_encodings_dict = pickle.load(f)
+
+def main():
+    image = input("image name: ")
+    file = cv2.imread(f'examples/{image}.jpeg')
+    test1_encoding = utils.face_encodings(file)
+    print(test1_encoding)
+    # load the encodings + names dictionary
+    # with open("encodings.pickle", "rb") as f:
+    #     name_encodings_dict = pickle.load(f)
 
 
-# load the input image
-image = cv2.imread("examples/test1.jpeg")
-# get the 128-d face embeddings for each face in the input image
-test1_encoding = utils.face_encodings(image)
+    # load the input image
+    # face_recognition_images.face_recognition_images(file, test1_encoding)`
 
+
+
+if __name__ == '__main__':
+    main()
