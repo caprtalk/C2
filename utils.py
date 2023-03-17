14bad73942e440b023f4908d089b982baa925b40
Caspar Brown <casparmoab@gmail.com>
2023-03-17 14:58:48 -0600

    commenting

--- utils.py
+++ utils.py
@@ -4,19 +4,17 @@ import cv2
 import numpy as np
 import os
 
-# load the face detector, landmark predictor, and face recognition model
+# load models
 face_detector = dlib.get_frontal_face_detector()
 shape_predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 face_encoder = dlib.face_recognition_model_v1("models/dlib_face_recognition_resnet_model_v1.dat")
 
-# change this to include other image formats you want to support (e.g. .webp)
 VALID_EXTENSIONS = ['.png', '.jpg', '.jpeg']
 
 def get_image_paths(root_dir, class_names):
     image_paths = []
     # loop over the class names
     for class_name in class_names:
-        # grab the paths to the files in the current class directory
         class_dir = os.path.sep.join([root_dir, class_name])
         class_file_paths = glob(os.path.sep.join([class_dir, '*.*']))
 
@@ -24,13 +22,10 @@ def get_image_paths(root_dir, class_names):
         for file_path in class_file_paths:
             # extract the file extension of the current file
             ext = os.path.splitext(file_path)[1]
-
-            # if the file extension is not in the valid extensions list, ignore the file
             if ext.lower() not in VALID_EXTENSIONS:
                 print("Skipping file: {}".format(file_path))
                 continue
 
-            # add the path to the current image to the list of image paths
             image_paths.append(file_path)
 
     return image_paths
@@ -57,9 +52,7 @@ def face_encodings(image):
 
 def nb_of_matches(known_encodings, unknown_encoding):
     # compute the Euclidean distance between the current face encoding
-    # and all the face encodings in the database
     distances = np.linalg.norm(known_encodings - unknown_encoding, axis=1)
     # keep only the distances that are less than the threshold
     small_distances = distances <= 0.6
-    # return the number of matches
     return sum(small_distances)
